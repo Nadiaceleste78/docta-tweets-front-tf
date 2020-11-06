244d1290baceddfd0dc1f817f2c12ed901451c5b
Your Name <you@example.com>
2020-11-06 19:02:52 -0300

    Se agregó Router

---
+++ src/components/ListaPosts.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    created () {
+        fetch("https://node-api-doctadevs.vercel.app/posts")
+    .then(response => response.json())
+
+    .then(result => {
+        if (result.error) return console.log (result);
+
+        this.posts = result.body;
+        return true;
+        }
+    )
+    
+    .catch(error => console.log('error', error));
+    },
+    data () {
+        return{
+            posts:[],
+        }
+    },
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <lista-post/>        
+    </div>
+</template>
+
+<script>
+
+    import ListaPosts from '@/components/ListaPosts';
+
+    export default {
+        name:"Home",
+        components: {ListaPosts}    
+    }
+</script>
